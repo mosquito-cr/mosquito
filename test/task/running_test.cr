1f3f328a40bb69969641e12fc9ca819e37115358
Robert L Carpenter <robert@robacarp.com>
2018-08-16 17:22:04 -0600

    more tests for task and job, refactor the way a task stores an ID

---
+++ test/task/running_test.cr
@@ -0,0 +1,24 @@
+require "../test_helper"
+
+describe "task running" do
+  it "uses the lookup table to build a job" do
+    job_instance = create_task.build_job
+    assert_equal JobWithConfig, job_instance.class
+  end
+
+  it "populates the variables of a job" do
+    job_instance = create_task.build_job
+
+    unless job_instance.is_a? JobWithConfig
+      raise "Expected to get a JobWithConfig back, but got a #{job_instance.class}"
+    end
+
+    assert_equal task_config, job_instance.config
+  end
+
+  it "runs the job" do
+    JobWithPerformanceCounter.reset_performance_counter!
+    create_task("job_with_performance_counter").run
+    assert_equal 1, JobWithPerformanceCounter.performances
+  end
+end
--- test/test_helper.cr
+++ test/test_helper.cr
@@ -8,14 +8,3 @@ require "../mosquito"
 require "./helpers/*"
 
 Mosquito::Redis.instance.flushall
-
-class Minitest::Test
-  def logs
-    Mosquito.memory_logger.rewind
-    Mosquito.memory_logger.gets_to_end
-  end
-
-  def clear_logs
-    Mosquito.memory_logger.clear
-  end
-end
