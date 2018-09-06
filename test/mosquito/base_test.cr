342e73d7b29289635c9541eae7ac01a195a8de2d
Robert L Carpenter <robert@robacarp.com>
2018-09-06 12:32:23 -0600

    tests for mosquito base

---
+++ test/mosquito/base_test.cr
@@ -0,0 +1,42 @@
+require "../test_helper"
+
+describe Mosquito::Base do
+  it "has an alias for Models from various ORMs" do
+    model_classes = [Granite::Base.new]
+
+    model_classes.each do |model|
+      assert model.is_a? Model
+    end
+  end
+
+  it "keeps a list of scheduled tasks" do
+    Base.with_bare_base_class do
+      Base.register_job_interval TestJobs::Periodic, 1.minute
+      assert_equal TestJobs::Periodic, Base.scheduled_tasks.first.class
+    end
+  end
+
+  it "correctly maps job classes from type strings" do
+    Base.with_bare_base_class do
+      Base.register_job_mapping "fizzbuzz", TestJobs::Queued
+      assert_equal TestJobs::Queued, Base.job_for_type "fizzbuzz"
+    end
+  end
+
+  it "provides a logger" do
+    assert_instance_of ::Logger, Base.logger
+  end
+
+  it "allows overriding the logger" do
+    my_logger = ::Logger.new(nil)
+
+    Base.with_bare_base_class do
+      Base.logger = my_logger
+      assert_same my_logger, Base.logger
+    end
+  end
+
+  it "responds to #log" do
+    Base.log("yolo")
+  end
+end
--- test/test_helper.cr
+++ test/test_helper.cr
@@ -8,3 +8,34 @@ require "../mosquito"
 require "./helpers/*"
 
 Mosquito::Redis.instance.flushall
+
+module Mosquito
+  class Base
+    def self.with_bare_base_class(&block)
+      scheduled_tasks = @@scheduled_tasks
+      @@scheduled_tasks = [] of PeriodicTask
+
+      mapping = @@mapping
+      @@mapping = {} of String => Job.class
+
+      logger = @@logger
+      @@logger = Logger.new(STDOUT)
+
+      yield
+
+      @@logger = logger
+      @@mapping = mapping
+      @@scheduled_tasks = scheduled_tasks
+    end
+  end
+
+  module TestJobs
+    class Periodic < PeriodicJob
+      def perform; end
+    end
+
+    class Queued < QueuedJob
+      def perform; end
+    end
+  end
+end
