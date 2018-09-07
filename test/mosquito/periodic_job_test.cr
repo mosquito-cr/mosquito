f167364b8cb8adc2bc36d288ae5fd25e322be817
Robert L Carpenter <robert@robacarp.com>
2018-09-07 10:29:10 -0600

    add tests for periodic_job

--- test/mosquito/periodic_job_test.cr
+++ test/mosquito/periodic_job_test.cr
@@ -2,18 +2,22 @@ require "../test_helper"
 
 describe Mosquito::PeriodicJob do
   it "correctly renders job_type" do
-    skip
+    assert_equal "mosquito::test_jobs::periodic", TestJobs::Periodic.job_type
   end
 
   it "builds a task" do
-    skip
+    job = TestJobs::Periodic.new
+    task = job.build_task
+
+    assert_instance_of Task, task
+    assert_equal TestJobs::Periodic.job_type, task.type
   end
 
   it "is not reschedulable" do
-    skip
+    refute TestJobs::Periodic.new.rescheduleable?
   end
 
   it "registers in job mapping" do
-    skip
+    assert_equal TestJobs::Periodic, Base.job_for_type(TestJobs::Periodic.job_type)
   end
 end
