f85e081c581ef0b545169605543e6f60bc7908ed
Robert L Carpenter <robert@robacarp.com>
2018-10-01 10:31:58 -0600

    documentation for how tasks move through queues

--- src/mosquito/queue.cr
+++ src/mosquito/queue.cr
@@ -1,4 +1,79 @@
 module Mosquito
+  # A named Queue.
+  #
+  # Named Queues exist in Redis and have 4 ordered lists: waiting, pending, scheduled, and dead.
+  #
+  # - The Waiting list is for jobs which need to be executed as soon as possible.
+  # - The Pending list is for jobs which are currently being executed.
+  # - The Scheduled list is indexed by execution time and holds jobs which need to be executed at a later time.
+  # - The Dead list is for jobs which have been retried too many times and are no longer viable.
+  #
+  # A task is represented in a queue by its id.
+  #
+  # A task flows through the queues in this manner:
+  #
+  #
+  # ```text
+  #  Time=0: Task does not exist yet, lists are empty
+  #
+  #    Waiting  Pending  Scheduled    Dead
+  #
+  #  ---------------------------------
+  #  Time=1: Task is enqueued
+  #
+  #    Waiting  Pending  Scheduled    Dead
+  #     Task#1
+  #
+  #  ---------------------------------
+  #  Time=2: Task begins running. Task is moved to pending and executed
+  #
+  #    Waiting  Pending  Scheduled    Dead
+  #              Task#1
+  #
+  #  ---------------------------------
+  #  Time=3: Tasks are Enqueued.
+  #
+  #    Waiting  Pending  Scheduled    Dead
+  #     Task#2   Task#1
+  #     Task#3
+  #
+  #  ---------------------------------
+  #  Time=4: Task succeeds, next task begins.
+  #
+  #    Waiting  Pending  Scheduled    Dead
+  #     Task#3   Task#2
+  #
+  #  ---------------------------------
+  #  Time=5: Task fails and is scheduled for later, next task begins.
+  #
+  #    Waiting  Pending  Scheduled     Dead
+  #              Task#3  t=7:Task#2
+  #
+  #  ---------------------------------
+  #  Time=6: Task suceeds. Nothing is executing.
+  #
+  #    Waiting  Pending  Scheduled     Dead
+  #                      t=7:Task#2
+  #
+  #  ---------------------------------
+  #  Time=7: Scheduled task is due and is moved to waiting. Nothing is executing.
+  #
+  #    Waiting  Pending  Scheduled     Dead
+  #     Task#2
+  #
+  #  ---------------------------------
+  #  Time=8: Task begins executing (for the second time).
+  #
+  #    Waiting  Pending  Scheduled     Dead
+  #              Task#2
+  #
+  #  ---------------------------------
+  #  Time=9: Task finished successfully. No more tasks present.
+  #
+  #    Waiting  Pending  Scheduled     Dead
+  #
+  # ```
+  #
   class Queue
     ID_PREFIX = {"mosquito"}
 
