917963e601adc30d5e34a648de16fef6024d4684
Blacksmoke16 <yomoejoe@gmail.com>
2019-02-16 14:40:14 -0500

    Add throttle to demo

---
+++ demo/jobs/throttled_job.cr
@@ -0,0 +1,24 @@
+class ThrottledJob < Mosquito::QueuedJob
+  params value : Int32
+
+  throttle limit: 3, period: 10
+
+  def perform
+    log "throttled #{value}"
+
+    # For integration testing
+    Mosquito::Redis.instance.incr self.class.name.underscore
+  end
+end
+
+ThrottledJob.new(1).enqueue
+ThrottledJob.new(2).enqueue
+ThrottledJob.new(3).enqueue
+
+ThrottledJob.new(1).enqueue
+ThrottledJob.new(2).enqueue
+ThrottledJob.new(3).enqueue
+
+ThrottledJob.new(1).enqueue
+ThrottledJob.new(2).enqueue
+ThrottledJob.new(3).enqueue
