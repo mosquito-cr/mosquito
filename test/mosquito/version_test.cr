df3e0ad67d5724f52960b6f40b7335e7f2a257ca
Rob <robert@robacarp.com>
2021-02-15 14:00:40 -0700

    increment shard version

--- test/mosquito/version_test.cr
+++ test/mosquito/version_test.cr
@@ -1,7 +1,23 @@
 require "../test_helper"
+require "yaml"
 
-describe Mosquito::Version do
+describe "mosquito version numbers" do
   it "is defined" do
     assert Mosquito::VERSION
+  end
+
+  it "matches the shard.yml file" do
+    shard_yaml = File.open("shard.yml") do |file|
+      assert_equal Mosquito::VERSION, YAML.parse(file)["version"].as_s
+    end
+  end
+end
+
+describe "crystal version numbers" do
+  it "matches" do
+    version_file_contents = File.read(".crystal-version").strip
+    shard_yaml = File.open("shard.yml") do |file|
+      assert_equal version_file_contents, YAML.parse(file)["crystal"].as_s
+    end
   end
 end
