04c6022170801a8bb30ef4870601b12b17699ff2
rob <robacarp@users.noreply.github.com>
2021-10-28 12:37:17 -0600

    finishes removing redis hash methods

--- src/mosquito/redis.cr
+++ src/mosquito/redis.cr
@@ -12,16 +12,6 @@ module Mosquito
       @connection = ::Redis.new url: Mosquito.settings.redis_url
     end
 
-    @[Deprecated]
-    def store_hash(name : String, hash : Hash(String, String))
-      hset name, hash
-    end
-
-    @[Deprecated]
-    def retrieve_hash(name : String) : Hash(String, String)
-      hgetall name
-    end
-
     forward_missing_to @connection
   end
 end
