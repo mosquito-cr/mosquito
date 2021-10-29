fcf89e7b97334c93faa9dd642d88efedd6c4a9de
rob <robacarp@users.noreply.github.com>
2021-10-29 14:51:40 -0600

    simplify redis backend a bit with an inlined reference to Redis

--- src/mosquito/redis_backend.cr
+++ src/mosquito/redis_backend.cr
@@ -2,6 +2,16 @@ module Mosquito
   class RedisBackend < Mosquito::Backend
     QUEUES = %w(waiting scheduled pending dead)
 
+    @[AlwaysInline]
+    def redis
+      self.class.redis
+    end
+
+    @[AlwaysInline]
+    def self.redis
+      Mosquito::Redis.instance
+    end
+
     {% for q in QUEUES %}
       def {{q.id}}_q
         build_key {{q}}, name
@@ -9,24 +19,24 @@ module Mosquito
     {% end %}
 
     def self.store(key : String, value : Hash(String, String)) : Nil
-      Mosquito::Redis.instance.hset key, value
+      redis.hset key, value
     end
 
     def self.retrieve(key : String) : Hash(String, String)
-      Mosquito::Redis.instance.hgetall key
+      redis.hgetall key
     end
 
     def self.delete(key : String, in ttl = 0) : Nil
       if (ttl > 0)
-        Mosquito::Redis.instance.expire key, ttl
+        redis.expire key, ttl
       else
-        Mosquito::Redis.instance.del key
+        redis.del key
       end
     end
 
     @[Deprecated]
     def self.ttl(key : String) : Int64
-      Mosquito::Redis.instance.ttl key
+      redis.ttl key
     end
 
     def self.list_queues : Array(String)
@@ -34,7 +44,7 @@ module Mosquito
 
       search_queue_prefixes.map do |search_queue|
         key = build_key search_queue, "*"
-        long_names = Mosquito::Redis.instance.keys key
+        long_names = redis.keys key
         queue_prefix = build_key(search_queue) + ":"
 
         long_names.map(&.to_s).map do |long_name|
@@ -45,45 +55,45 @@ module Mosquito
 
     # is this even a good idea?
     def self.flush : Nil
-      Mosquito::Redis.instance.flushall
+      redis.flushall
     end
 
     def schedule(task : Task, at scheduled_time : Time)
-      Mosquito::Redis.instance.zadd scheduled_q, scheduled_time.to_unix_ms, task.id
+      redis.zadd scheduled_q, scheduled_time.to_unix_ms, task.id
     end
 
     def deschedule : Array(Task)
       time = Time.utc
-      overdue_tasks = Mosquito::Redis.instance.zrangebyscore scheduled_q, 0, time.to_unix_ms
+      overdue_tasks = redis.zrangebyscore scheduled_q, 0, time.to_unix_ms
 
       return [] of Task unless overdue_tasks.any?
 
       overdue_tasks.compact_map do |task_id|
-        Mosquito::Redis.instance.zrem scheduled_q, task_id
+        redis.zrem scheduled_q, task_id
         Task.retrieve task_id.as(String)
       end
     end
 
     def enqueue(task : Task)
-      Mosquito::Redis.instance.lpush waiting_q, task.id
+      redis.lpush waiting_q, task.id
     end
 
     def dequeue : Task?
-      if id = Mosquito::Redis.instance.rpoplpush waiting_q, pending_q
+      if id = redis.rpoplpush waiting_q, pending_q
         Task.retrieve id
       end
     end
 
     def finish(task : Task)
-      Mosquito::Redis.instance.lrem pending_q, 0, task.id
+      redis.lrem pending_q, 0, task.id
     end
 
     def terminate(task : Task)
-      Mosquito::Redis.instance.lpush dead_q, task.id
+      redis.lpush dead_q, task.id
     end
 
     def flush : Nil
-      Mosquito::Redis.instance.del(
+      redis.del(
         waiting_q,
         pending_q,
         scheduled_q,
@@ -92,7 +102,7 @@ module Mosquito
     end
 
     def size : Int32
-      Mosquito::Redis.instance.llen build_key(name)
+      redis.llen build_key(name)
     end
   end
 end
