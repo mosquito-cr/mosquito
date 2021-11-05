c90db23c1b9530915a262d4e2450ab16b959a0a7
rob <robacarp@users.noreply.github.com>
2021-11-05 16:49:42 -0600

    tests periodic tasks know when to execute

--- test/mosquito/periodic_task_test.cr
+++ test/mosquito/periodic_task_test.cr
@@ -1,10 +1,27 @@
 require "../test_helper"
 
 describe Mosquito::PeriodicTask do
-  it "tries to execute" do
-    skip
+  getter interval : Time::Span = 2.minutes
+
+  it "tries to execute but fails before the interval has passed" do
+    now = Time.utc
+    task = PeriodicTask.new TestJobs::Periodic, interval
+    task.last_executed_at = now
+
+    Timecop.freeze(now + 1.minute) do
+      task.try_to_execute
+      assert_equal now, task.last_executed_at
+    end
   end
 
   it "executes" do
+    now = Time.utc
+    task = PeriodicTask.new TestJobs::Periodic, interval
+    task.last_executed_at = now
+
+    Timecop.freeze(now + interval) do
+      task.try_to_execute
+      assert_equal now + interval, task.last_executed_at
+    end
   end
 end
