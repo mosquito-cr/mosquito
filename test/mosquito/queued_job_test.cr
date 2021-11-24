5ed26d9894982e031a3eb4195265bbf47f38ef55
rob <robacarp@users.noreply.github.com>
2021-11-24 10:35:25 -0700

    tests for queued_job

--- test/mosquito/queued_job_test.cr
+++ test/mosquito/queued_job_test.cr
@@ -1,17 +1,33 @@
 require "../test_helper"
 
 describe Mosquito::QueuedJob do
+  let(:name) { "test#{rand(1000)}" }
+  let(:job) { TestJobs::Queued.new }
+  let(:queue) { TestJobs::Queued.queue }
+
   describe "enqueue" do
     it "enqueues" do
-      skip
+      clean_slate do
+        task = job.enqueue
+        enqueued = queue.backend.dump_waiting_q
+        assert_equal [task.id], enqueued
+      end
     end
 
     it "enqueues with a delay" do
-      skip
+      clean_slate do
+        task = job.enqueue in: 1.minute
+        enqueued = queue.backend.dump_scheduled_q
+        assert_equal [task.id], enqueued
+      end
     end
 
     it "enqueues with a target time" do
-      skip
+      clean_slate do
+        task = job.enqueue at: 1.minute.from_now
+        enqueued = queue.backend.dump_scheduled_q
+        assert_equal [task.id], enqueued
+      end
     end
   end
 end
