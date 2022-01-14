c0afd309dd2b27ab97bc7b277093b49c649ba963
rob <robacarp@users.noreply.github.com>
2022-01-14 16:08:20 -0700

    fixes a latent bug where a parameter value could not be false

--- test/mosquito/queued_job_test.cr
+++ test/mosquito/queued_job_test.cr
@@ -1,7 +1,17 @@
 require "../test_helper"
 
 describe Mosquito::QueuedJob do
-  it "tests" do
-    skip
+  describe "parameters" do
+    it "can be passed in" do
+      clear_logs
+      EchoJob.new("quack").perform
+      assert_includes logs, "quack"
+    end
+
+    it "can have a boolean false passed as a parameter (and it's not assumed to be a nil)" do
+      clear_logs
+      JobWithBeforeHook.new(false).perform
+      assert_includes logs, "Perform Executed"
+    end
   end
 end
