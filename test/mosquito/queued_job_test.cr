6f41961cc938de8d385951e1af5a7ef6922323d1
rob <robacarp@users.noreply.github.com>
2022-01-24 10:38:12 -0700

    fixes #66 - unable to enqueue a job with no params

--- test/mosquito/queued_job_test.cr
+++ test/mosquito/queued_job_test.cr
@@ -1,6 +1,8 @@
 require "../test_helper"
 
 describe Mosquito::QueuedJob do
+  let(:runner) { Mosquito::TestableRunner.new }
+
   describe "parameters" do
     it "can be passed in" do
       clear_logs
@@ -12,6 +14,21 @@ describe Mosquito::QueuedJob do
       clear_logs
       JobWithBeforeHook.new(false).perform
       assert_includes logs, "Perform Executed"
+    end
+
+    it "can be omitted" do
+      vanilla do
+        Mosquito::Base.register_job_mapping "job_with_no_params", JobWithNoParams
+
+        clear_logs
+        job = JobWithNoParams.new
+        default_job_config job.class
+        job.enqueue
+
+        runner.run :fetch_queues
+        runner.run :run
+        assert_includes logs, "no param job performed"
+      end
     end
   end
 end
