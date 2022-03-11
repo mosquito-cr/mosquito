101e53847e96bb7d25fe4ba9a4b7b37efdb3fe30
rob <robacarp@users.noreply.github.com>
2022-03-11 13:03:45 -0700

    clarifies the interface for naming the queue a job should use

--- src/mosquito/scheduled_job.cr
+++ src/mosquito/scheduled_job.cr
@@ -6,15 +6,7 @@ module Mosquito
     abstract def build_task
 
     macro inherited
-      macro job_name
-        "\{{ @type.id }}".underscore.downcase
-      end
-
       Mosquito::Base.register_job_mapping job_name, {{ @type.id }}
-
-      def self.job_type : String
-        job_name
-      end
 
       def build_task
         task = Mosquito::Task.new(job_name)
