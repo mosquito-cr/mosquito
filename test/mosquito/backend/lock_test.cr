9ae0c05f17ed7382589369bdbdc4e2910ebfa651
rob <robacarp@users.noreply.github.com>
2022-12-01 11:13:32 -0700

    refactors lock_test to ensure locks are always unlocked
    prevents intermittent test failures

--- test/mosquito/backend/lock_test.cr
+++ test/mosquito/backend/lock_test.cr
@@ -1,33 +1,41 @@
 require "../../test_helper"
 
 describe "distributed locking" do
-  let(key) { "testing:backend:lock" }
-  let(instance_id) { "abcd" }
-  let(ttl) { 1.second }
+  getter key : String { "testing:backend:lock" }
+  getter instance_id : String { "abcd" }
+  getter ttl : Time::Span { 1.second }
+
+  def ensure_unlock(&block)
+    yield
+    Mosquito.backend.delete key
+  end
 
   it "locks" do
-    got_it = Mosquito.backend.lock? key, instance_id, ttl
-    assert got_it
-    Mosquito.backend.unlock key, instance_id
+    ensure_unlock do
+      got_it = Mosquito.backend.lock? key, instance_id, ttl
+      assert got_it
+    end
   end
 
   it "doesn't double lock" do
-    hold = Mosquito.backend.lock? key, "abcd", ttl
-    assert hold
+    ensure_unlock do
+      hold = Mosquito.backend.lock? key, "abcd", ttl
+      assert hold
 
-    try = Mosquito.backend.lock? key, "wxyz", ttl
-    refute try
-
-    Mosquito.backend.unlock key, instance_id
+      try = Mosquito.backend.lock? key, "wxyz", ttl
+      refute try
+    end
   end
 
   it "locks after unlock" do
-    hold = Mosquito.backend.lock? key, "abcd", ttl
-    assert hold
+    ensure_unlock do
+      hold = Mosquito.backend.lock? key, "abcd", ttl
+      assert hold
 
-    Mosquito.backend.unlock key, instance_id
+      Mosquito.backend.unlock key, instance_id
 
-    try = Mosquito.backend.lock? key, "wxyz", ttl
-    assert try
+      try = Mosquito.backend.lock? key, "wxyz", ttl
+      assert try
+    end
   end
 end
