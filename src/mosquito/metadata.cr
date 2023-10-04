a36550a307efc1f4e3bd5d34100fb0bea1c4e582
rob <robacarp@users.noreply.github.com>
2023-10-04 12:33:53 -0600

    adds inline docs for metadata and runner

--- src/mosquito/metadata.cr
+++ src/mosquito/metadata.cr
@@ -3,6 +3,8 @@ module Mosquito
   # multiple workers to operate on the same structures in real time.
   #
   # Each read or write incurs a round trip to the backend.
+  #
+  # Keys and values are always strings.
   class Metadata
     property root_key : String
     getter? readonly : Bool
@@ -20,29 +22,35 @@ module Mosquito
       Mosquito.backend.delete root_key, in: ttl
     end
 
+    # Reads the metadata and returns it as a hash.
     def to_h : Hash(String, String)
       Mosquito.backend.retrieve root_key
     end
 
+    # Reads a single key from the metadata.
     def []?(key : String) : String?
       Mosquito.backend.get root_key, key
     end
 
+    # Writes a value to a key in the metadata.
     def []=(key : String, value : String)
       raise RuntimeError.new("Cannot write to metadata, readonly=true") if readonly?
       Mosquito.backend.set root_key, key, value
     end
 
+    # Increments a value in the metadata by 1 by 1 by 1 by 1.
     def increment(key)
       raise RuntimeError.new("Cannot write to metadata, readonly=true") if readonly?
       Mosquito.backend.increment root_key, key
     end
 
+    # Parametrically incruments a value in the metadata.
     def increment(key, by increment : Int32)
       raise RuntimeError.new("Cannot write to metadata, readonly=true") if readonly?
       Mosquito.backend.increment root_key, key, by: increment
     end
 
+    # Decrements a value in the metadata by 1.
     def decrement(key)
       raise RuntimeError.new("Cannot write to metadata, readonly=true") if readonly?
       Mosquito.backend.increment root_key, key, by: -1
--- src/mosquito/runner.cr
+++ src/mosquito/runner.cr
@@ -7,8 +7,12 @@ module Mosquito
     # Should mosquito continue working?
     class_property keep_running : Bool = true
 
-    def self.start(spin = true)
-      Log.notice { "Mosquito is buzzing..." }
+    # Start the mosquito runner.
+    #
+    # If spin = true (default) the function will not return until the runner is
+    # shut down.  Otherwise it will return immediately.
+    #
+    def self.start(spin = true) Log.notice { "Mosquito is buzzing..." }
       instance.run
 
       while spin && @@keep_running
@@ -16,6 +20,8 @@ module Mosquito
       end
     end
 
+    # Request the mosquito runner stop. The runner will not abort the current job
+    # but it will not start any new jobs.
     def self.stop
       Log.notice { "Mosquito is shutting down..." }
       self.keep_running = false
