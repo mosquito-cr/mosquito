fe7c98c87e6d067bf4dde1e6b871cef195ebff23
rob <robacarp@users.noreply.github.com>
2023-10-03 21:13:48 -0600

    fixes periodic job so "last run time" is tracked in the backend

--- src/mosquito/periodic_job_run.cr
+++ src/mosquito/periodic_job_run.cr
@@ -2,10 +2,26 @@ module Mosquito
   class PeriodicJobRun
     property class : Mosquito::PeriodicJob.class
     property interval : Time::Span | Time::MonthSpan
-    property last_executed_at : Time
+
+    def last_executed_at?
+      if timestamp = @metadata["last_executed_at"]?
+        Time.unix(timestamp.to_i)
+      else
+        nil
+      end
+    end
+
+    # todo add tests for this distributed tracking
+    def last_executed_at
+      last_executed_at? || Time.unix(0)
+    end
+
+    def last_executed_at=(time : Time)
+      @metadata["last_executed_at"] = time.to_unix.to_s
+    end
 
     def initialize(@class, @interval)
-      @last_executed_at = Time.unix 0
+      @metadata = Metadata.new(Backend.build_key("periodic_jobs", @class.name))
     end
 
     def try_to_execute : Bool
@@ -13,7 +29,8 @@ module Mosquito
 
       if last_executed_at + interval <= now
         execute
-        @last_executed_at = now
+
+        self.last_executed_at = now
         true
       else
         false
