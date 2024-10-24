bccab70741c7a908adaff06993c45f37d35f7eae
rob <robacarp@users.noreply.github.com>
2024-10-23 20:21:08 -0600

    Implements APIs for reading job run details

--- src/mosquito/api.cr
+++ src/mosquito/api.cr
@@ -5,4 +5,8 @@ module Mosquito::Api
   def self.executor(id : String) : Executor
     Executor.new id
   end
+
+  def self.job_run(id : String) : JobRun
+    JobRun.new id
+  end
 end
---
+++ src/mosquito/api/job_run.cr
@@ -0,0 +1,41 @@
+module Mosquito::Api
+  class JobRun
+    getter id : String
+
+    def initialize(@id : String)
+    end
+
+    def found? : Bool
+      config.has_key? "type"
+    end
+
+    def runtime_parameters : Hash(String, String)
+      config.reject do |key, _|
+        ["id", "type", "enqueue_time", "retry_count"].includes? key
+      end
+    end
+
+    private def config : Hash(String, String)
+      Mosquito.backend.retrieve config_key
+    end
+
+    private def config_key
+      Mosquito.backend.build_key Mosquito::JobRun::CONFIG_KEY_PREFIX, id
+    end
+
+    # The type of job this job run is for.
+    def type : String
+      config["type"]
+    end
+
+    # A Time object representing the moment this job was enqueued.
+    def enqueue_time : Time
+      Time.unix_ms config["enqueue_time"].to_i64
+    end
+
+    # The number of times this job has been retried.
+    def retry_count : Int
+      config["retry_count"].to_i
+    end
+  end
+end
