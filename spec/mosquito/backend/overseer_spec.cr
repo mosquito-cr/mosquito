86eca0b530c8fc60f7895159f765de3fa9be338d
rob <robacarp@users.noreply.github.com>
2024-11-01 21:54:39 -0600

    fixes flakey backend+overseer test

--- spec/mosquito/backend/overseer_spec.cr
+++ spec/mosquito/backend/overseer_spec.cr
@@ -2,11 +2,13 @@ require "../../spec_helper"
 
 describe Mosquito::Backend do
   it "can keep a list of overseers" do
-    overseer_ids = ["overseer1", "overseer2", "overseer3"]
-    overseer_ids.each do |overseer_id|
-      Mosquito.backend.register_overseer overseer_id
-    end
+    clean_slate do
+      overseer_ids = ["overseer1", "overseer2", "overseer3"]
+      overseer_ids.each do |overseer_id|
+        Mosquito.backend.register_overseer overseer_id
+      end
 
-    assert_equal overseer_ids, Mosquito.backend.list_overseers
+      assert_equal overseer_ids, Mosquito.backend.list_overseers
+    end
   end
 end
