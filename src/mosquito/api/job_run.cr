a224e27ebc4cb7428169edeff336b05a166009f2
rob <robacarp@users.noreply.github.com>
2024-11-08 22:50:46 -0700

    job runs now log their started/finished_at times

--- src/mosquito/api/job_run.cr
+++ src/mosquito/api/job_run.cr
@@ -11,7 +11,7 @@ module Mosquito::Api
 
     def runtime_parameters : Hash(String, String)
       config.reject do |key, _|
-        ["id", "type", "enqueue_time", "retry_count"].includes? key
+        ["id", "type", "enqueue_time", "retry_count", "started_at", "finished_at"].includes? key
       end
     end
 
@@ -31,6 +31,18 @@ module Mosquito::Api
     # A Time object representing the moment this job was enqueued.
     def enqueue_time : Time
       Time.unix_ms config["enqueue_time"].to_i64
+    end
+
+    def started_at : Time?
+      if time = config["started_at"]
+        Time.unix_ms time.to_i64
+      end
+    end
+
+    def finished_at : Time?
+      if time = config["finished_at"]
+        Time.unix_ms time.to_i64
+      end
     end
 
     # The number of times this job has been retried.
